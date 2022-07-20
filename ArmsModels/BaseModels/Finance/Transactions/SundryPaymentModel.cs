89ee347e14abfa1a6747624312e79a246f1bb5aa
Rijas Laptop <mkrijas@gmail.com>
2022-07-20 09:48:39 +0530

    Created model for SundryPayment

---
+++ ArmsModels/BaseModels/Finance/Transactions/SundryPaymentModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArmsModels.BaseModels.Finance.Transactions
+{
+    public class SundryPaymentModel:TransactionBaseModel
+    {
+        public int? SundryPaymentID { get; set; }
+        [Required]
+        public string PaymentMode { get; set; }
+        [Required]
+        public int? CoaID { get; set; }
+        public virtual string AccountName { get; set; }
+        public string Reference { get; set; }
+        public List<SundryPaymentEntryModel> Entries { get; set; }
+
+    }
+
+    public class SundryPaymentEntryModel
+    {
+        public long? ID { get; set; }
+        public int? ParentID { get; set; }
+        public int? BranchID { get; set; }
+        [Required]
+        public int? CoaID { get;}
+        public virtual string AccountName { get; set; }
+        [Required]
+        public decimal? Amount { get; set; }
+        public string Rederence { get; set; }
+    }
+}
