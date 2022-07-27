44164097c1730091ac32403d6ce5c8fbb6dcbe37
mkrijas <mkrijas@gmail.com>
2022-07-27 10:21:32 +0530

    Created Finance ContraModel

---
+++ ArmsModels/BaseModels/Finance/Transactions/ContraModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArmsModels.BaseModels
+{
+    public class ContraModel: TransactionBaseModel
+    {
+        public int? ContraID { get; set; }
+        public string ContraModeHome { get; set; }
+        public int? CoaIDHome { get; set; }
+        public int? BranchIDOther { get; set; }
+        public string ContraModeOther { get; set; }
+        public int? CoaIDOther { get; set; }
+        public string EntryReference { get; set; }        
+
+    }
+}
