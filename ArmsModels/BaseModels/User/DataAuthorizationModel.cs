74709437700f72128acabfb3f1b340c043da993c
mkrijas <mkrijas@gmail.com>
2022-11-17 14:38:08 +0530

    AuthSettings Modified

--- ArmsModels/BaseModels/User/DataAuthorizationModel.cs
+++ ArmsModels/BaseModels/User/DataAuthorizationModel.cs
@@ -30,6 +30,8 @@ namespace ArmsModels.BaseModels
         public int? AuthorizeTypeID { get; set; }
         [Required]
         public int? DocTypeID { get; set; }
+        [Required]
+        public string RoleID { get; set; }
         public SharedModels.UserInfoModel UserInfo { get; set; }
     }
 
