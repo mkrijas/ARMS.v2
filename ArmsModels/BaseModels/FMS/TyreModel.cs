6bccbea840ce6d23600c9692949dfb952d4770cd
Rijas Laptop <mkrijas@gmail.com>
2023-02-10 19:29:22 +0530

    TyreModel Created

---
+++ ArmsModels/BaseModels/FMS/TyreModel.cs
@@ -0,0 +1,20 @@
+﻿using ArmsModels.SharedModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArmsModels.BaseModels
+{
+    public class TyreModel
+    {
+        public int? TyreID { get; set; }
+        public string TyreSerialNumber { get; set; }
+        public string Make { get; set; } // Hint: MRF , CEAT
+
+        public int? AssetID { get; set; }
+        [Required]
+        public string TyreType { get; set; } // Front/ Back/ All-Position
+        [Required]
+        public string TyreSize { get; set; } // 1000 x 25 etc
+        public bool Tubeless { get; set; } = false;
+        public UserInfoModel UserInfo { get; set; } = new();
+    }
+}
---
+++ ArmsServices/DataServices/FMS/TyreService.cs
@@ -0,0 +1,6 @@
+﻿namespace ArmsServices.DataServices.FMS
+{
+    public class TyreService
+    {
+    }
+}
