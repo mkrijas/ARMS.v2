981196b2441abfe342f106ac52ab88fdf2ad76cd
Anjoom <anjoommuhammedhidas@gmail.com>
2023-04-17 15:34:53 +0530

    Register redirect modified.

--- Views/Pages/UserAccount/Login.cshtml.cs
+++ Views/Pages/UserAccount/Login.cshtml.cs
@@ -56,14 +56,14 @@ namespace Views.Pages.UserAccount
             public bool RememberMe { get; set; }
         }
 
-        public async Task OnGetAsync(string returnUrl = null)
+        public async Task OnGetAsync(string returnUrl = "~/UserAccount/Login")
         {
             if (!string.IsNullOrEmpty(ErrorMessage))
             {
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
 
-            returnUrl ??= Url.Content("~/UserAccount/Login");
+            returnUrl ??= Url.Content("~/");
 
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
@@ -75,7 +75,7 @@ namespace Views.Pages.UserAccount
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/UserAccount/Login");
+            returnUrl ??= Url.Content("~/");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         
@@ -105,6 +105,7 @@ namespace Views.Pages.UserAccount
                 }
                 else
                 {
+
                     ModelState.AddModelError(string.Empty, "Invalid login attempt.");
                     return Page();
                 }                
