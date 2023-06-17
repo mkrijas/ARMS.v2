5376ecaf9c8f6f022cb330d7164addb8b24a7cbb
Rijas Laptop <mkrijas@gmail.com>
2023-06-17 10:18:40 +0530

    Breakdown dateTime format bug cleared

--- ArmsModels/BaseModels/Finance/Transactions/InventoryReleaseModel.cs
+++ ArmsModels/BaseModels/Finance/Transactions/InventoryReleaseModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
-using ArmsModels.SharedModels
+using ArmsModels.SharedModels;
 
 namespace ArmsModels.BaseModels
 {
