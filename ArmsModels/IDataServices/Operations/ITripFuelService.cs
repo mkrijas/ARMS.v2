24b07e192041e1f8a0e3623fc2dc8323c5c7dcbf
ROHITH MP <rohithmp2014@gmail.com>
2023-07-15 13:06:43 +0530

    ITripFuelService => TripFuelDialogue, TripDetails updated with Permission.

--- ArmsModels/IDataServices/Operations/ITripFuelService.cs
+++ ArmsModels/IDataServices/Operations/ITripFuelService.cs
@@ -11,8 +11,8 @@ namespace ArmsServices.DataServices
 {
     public interface ITripFuelService
     {
-        TripFuelModel Update(TripFuelModel model);
-        int Delete(int? TripFuelID, string UserID);
+        TripFuelModel Update(TripFuelModel model);  //Edit
+        int Delete(int? TripFuelID, string UserID);  //Edit
         TripFuelModel Select(int? TripFuelID);
         IEnumerable<TripFuelModel> SelectByTrip(long? TripID);
     }
