9a1da86158180eacc6d6e971a577bb891b1bfb30
Hafis <hafizappu1@gmail.com>
2023-08-24 10:47:49 +0530

    Modify Inventory Item Model

---
+++ ArmsModels/BaseModels/Inventory/InventoryQtyManageModel.cs
@@ -0,0 +1,15 @@
+﻿using ArmsModels.BaseModels;
+
+namespace Core.BaseModels.Inventory
+{
+    public class InventoryQtyManageModel
+    {
+        public int? QtyManageID { get; set; }
+        public StoreModel Store { get; set; }
+        public InventoryItemModel Item { get; set; }
+        public decimal? MinQty { get; set; }
+        public decimal? ReOrderQty { get; set; }
+        public decimal? InhandQty { get; set; }
+
+    }
+}
