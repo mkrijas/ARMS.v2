309208bcb6a22a508c654d10f706260bbe10f540
Hafis <hafizappu1@gmail.com>
2023-09-01 10:13:10 +0530

    Asseet transfer truck event updated

--- ArmsModels/BaseModels/Inventory/InventoryQtyManageModel.cs
+++ ArmsModels/BaseModels/Inventory/InventoryQtyManageModel.cs
@@ -1,15 +1,21 @@
 ﻿using ArmsModels.BaseModels;
+using ArmsModels.SharedModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace Core.BaseModels.Inventory
-{
+namespace ArmsModels.BaseModels
+{ 
     public class InventoryQtyManageModel
     {
         public int? QtyManageID { get; set; }
         public StoreModel Store { get; set; }
-        public InventoryItemModel Item { get; set; }
+        public InventoryItemModel InventoryItem { get; set; }
         public decimal? MinQty { get; set; }
         public decimal? ReOrderQty { get; set; }
         public decimal? InhandQty { get; set; }
-
+        public UserInfoModel UserInfo { get; set; }
     }
 }
