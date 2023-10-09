20e22a3bcf7bbca6c98177dd72ec5bd729c29a4a
ROHITH MP <rohithmp2014@gmail.com>
2023-10-09 15:25:13 +0530

    UnaprovedDialog implemented with clone.

--- ArmsModels/BaseModels/User/UserModel.cs
+++ ArmsModels/BaseModels/User/UserModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -7,19 +8,24 @@ using System.Threading.Tasks;
 namespace ArmsModels.BaseModels
 {
     public class UserModel
-    {       
+    {
         [Required]
-            public string UserID { get; set; }
+        public string UserID { get; set; }
         [Required]
-            public string UserName { get; set; }  
-            public string Email { get; set; }     
-            public bool EmailConfirmed { get; set; }
-            public string PasswordHash { get; set; }
-            public string PhoneNumber { get; set; }
-            public bool PhoneNumberConfirmed { get; set; }
-            public bool TwoFactorEnabled { get; set; }
-            public byte? RecordStatus { set; get; }
-            public string UpdatedBy { get; set; }
-            public int? LoggedBranch { get; set; }               
-    }  
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string PasswordHash { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+        public byte? RecordStatus { set; get; }
+        public string UpdatedBy { get; set; }
+        public int? LoggedBranch { get; set; }
+        public object Clone()
+        {
+            string Json = JsonConvert.SerializeObject(this);
+            return JsonConvert.DeserializeObject<UserModel>(Json);
+        }
+    }
 }
