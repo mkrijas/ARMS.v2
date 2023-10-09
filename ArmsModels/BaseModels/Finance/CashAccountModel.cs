ffef08171c608b2ea8fdb48b00f77d4449e7442c
ROHITH MP <rohithmp2014@gmail.com>
2023-10-09 15:03:00 +0530

    StoreView implemented with clone.

--- ArmsModels/BaseModels/Finance/CashAccountModel.cs
+++ ArmsModels/BaseModels/Finance/CashAccountModel.cs
@@ -11,16 +11,14 @@ namespace ArmsModels.BaseModels
     {
         public int? CashAccountID { get; set; }
         [Required]
-        public string Title { get; set; }        
+        public string Title { get; set; }
         public string CashCode { get; set; }
         [Required]
         public ChartOfAccountModel Coa { get; set; } = new();
-      
         public int? BranchID { get; set; }
         public decimal? MinBalance { get; set; }
         public decimal? MaxBalance { get; set; }
         public bool? IsDisabled { get; set; }
-
         public object Clone()
         {
             string Json = JsonConvert.SerializeObject(this);
