c29c084c0dd2ac673e5d29da148ed4a7a920779f
ROHITH MP <rohithmp2014@gmail.com>
2023-10-09 16:05:25 +0530

    AssetTransferView implemented with clone.

--- ArmsModels/BaseModels/Finance/Transactions/OperationTransactionModel.cs
+++ ArmsModels/BaseModels/Finance/Transactions/OperationTransactionModel.cs
@@ -1,5 +1,6 @@
 ﻿using ArmsModels.SharedModels;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -8,10 +9,10 @@ using System.Threading.Tasks;
 
 namespace ArmsModels.BaseModels
 {
-    public class OpTranModel:TransactionBaseModel
+    public class OpTranModel : TransactionBaseModel, ICloneable
     {
         public OpTranModel()
-        {            
+        {
             Transactions = new();
         }
         public int? OpTranID { get; set; }
@@ -22,28 +23,33 @@ namespace ArmsModels.BaseModels
         public virtual int? CreditCoaID { get; set; }
         [Required]
         public string Area { get; set; } // Operation,Maintenenace req
-        public long? TripID { get; set; } 
+        public long? TripID { get; set; }
         public int? AssetTransferID { get; set; }
         public int? TruckID { get; set; }
         public int? JobCardID { get; set; }
         [ValidateComplexType]
         public List<OpTranSubModel> Transactions { get; set; } = new();
         public virtual TripInfoModel TripInfo { get; set; }  // select   
+        public object Clone()
+        {
+            string Json = JsonConvert.SerializeObject(this);
+            return JsonConvert.DeserializeObject<OpTranModel>(Json);
+        }
     }
 
 
     public class OpTranSubModel
     {
         public long? OpTranSubID { get; set; }
-        public long? OpTranID { get; set; } 
+        public long? OpTranID { get; set; }
         [Required]
         public string ExpenseUsageCode { get; set; }
         public int? CoaID { get; set; }
-        public string Reference { get; set; }        
+        public string Reference { get; set; }
         [Required]
         [Range(1, double.MaxValue, ErrorMessage = "Must have Non-Zero Value {1}")]
-        public decimal? Amount { get; set; }        
-        public decimal? Quantity { get; set; }        
-        public string Unit { get; set; }           
+        public decimal? Amount { get; set; }
+        public decimal? Quantity { get; set; }
+        public string Unit { get; set; }
     }
 }
