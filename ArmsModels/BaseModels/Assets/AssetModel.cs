1a260c24519c3c537ebc9f77654c6e2760378419
ROHITH MP <rohithmp2014@gmail.com>
2023-10-09 16:23:41 +0530

    AssetDisplayConrtol implemented with clone.

--- ArmsModels/BaseModels/Assets/AssetModel.cs
+++ ArmsModels/BaseModels/Assets/AssetModel.cs
@@ -5,13 +5,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using ArmsModels.SharedModels;
 using ExpressiveAnnotations.Attributes;
-
+using Newtonsoft.Json;
 
 namespace ArmsModels.BaseModels
 {
-
     public class AssetModel
     {
+        public object Clone()
+        {
+            string Json = JsonConvert.SerializeObject(this);
+            return JsonConvert.DeserializeObject<AssetModel>(Json);
+        }
         public int? AssetID { get; set; }
         [Required]
         public string Description { get; set; }
@@ -68,10 +72,6 @@ namespace ArmsModels.BaseModels
         public bool Scrap { get; set; } = false;
         public string Status { get; set; }//Scrap,Dismantled,Sold,Revaluated        
         public UserInfoModel UserInfo { get; set; } = new();
-        //Additional Methods Required are 
-        //Revaluation
-        //Dismantle
-        //Sale        
     }
 
     public class AssetViewModel
@@ -81,15 +81,14 @@ namespace ArmsModels.BaseModels
         public List<AssetViewModel> Children { get; set; } = new();
     }
 
-
     public class AssetClassModel
     {
         public int? AssetClassID { get; set; }
         public string AssetClassName { get; set; }
         public int? PostingGroupID { get; set; }
         public UserInfoModel UserInfo { get; set; } = new();
-
     }
+
     public class AssetSubClassModel
     {
         public int? ID { get; set; }
@@ -100,6 +99,7 @@ namespace ArmsModels.BaseModels
         public int? AssetClassID { get; set; }
         public UserInfoModel UserInfo { get; set; } = new();
     }
+
     public class AssetStatusUpdateModel
     {
         public int? StatusUpdateID { get; set; }
@@ -152,6 +152,4 @@ namespace ArmsModels.BaseModels
         public int? AccountTransactionID { get; set; }
         public UserInfoModel UserInfo { get; set; } = new();
     }
-
-
 }
