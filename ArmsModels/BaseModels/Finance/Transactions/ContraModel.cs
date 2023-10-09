6d923510140de6125310a5d1bc45e94cc0eaf5c5
ROHITH MP <rohithmp2014@gmail.com>
2023-10-09 19:10:53 +0530

    ReceiptModel implemented with clone.

--- ArmsModels/BaseModels/Finance/Transactions/ContraModel.cs
+++ ArmsModels/BaseModels/Finance/Transactions/ContraModel.cs
@@ -7,32 +7,31 @@ using Newtonsoft.Json;
 
 namespace ArmsModels.BaseModels
 {
-    public class ContraModel: TransactionBaseModel, ICloneable
+    public class ContraModel : TransactionBaseModel, ICloneable
     {
+        public object Clone()
+        {
+            string Json = JsonConvert.SerializeObject(this);
+            return JsonConvert.DeserializeObject<ContraModel>(Json);
+        }
         public int? ContraID { get; set; }
         [Required]
         public string ContraModeHome { get; set; }
         [Required]
         public string ArdCodeHome { get; set; }
         [Required]
-        public int? CoaIDHome { get; set; }       
+        public int? CoaIDHome { get; set; }
         [Required]
         public string ContraModeOther { get; set; }
         [Required]
         public string ArdCodeOther { get; set; }
         [Required]
-        public int? CoaIDOther { get; set; }        
+        public int? CoaIDOther { get; set; }
         public string PaymentTool { get; set; }
         public decimal? BankCharges { get; set; }
         [RequiredIf("PaymentTool", "CHEQUE", ErrorMessage = "Cheque details must be specified")]
         public ChequeModel ChequeInfo { get; set; } = new();
         public string EntryReference { get; set; }
         public bool IsPayment { get; set; }
-        public object Clone()
-        {
-            string Json = JsonConvert.SerializeObject(this);
-            return JsonConvert.DeserializeObject<ContraModel>(Json);
-        }
-
     }
 }
