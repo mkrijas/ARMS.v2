d2406bcd5f34a8c4bdcddab7f05a3bded914d246
ROHITH MP <rohithmp2014@gmail.com>
2023-10-12 23:28:28 +0530

    TruckModel, TruckRegistrationModel, TruckTransferModel and TruckTypeModel implemented with clone.

--- ArmsModels/BaseModels/FMS/TruckTransferModel.cs
+++ ArmsModels/BaseModels/FMS/TruckTransferModel.cs
@@ -1,15 +1,22 @@
 ﻿using ArmsModels.SharedModels;
+using Newtonsoft.Json;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace ArmsModels.BaseModels
 {
-    public class TruckTransferInitiationModel
-    {        
+    public class TruckTransferInitiationModel : ICloneable
+    {
+        public object Clone()
+        {
+            string Json = JsonConvert.SerializeObject(this);
+            return JsonConvert.DeserializeObject<TruckTransferInitiationModel>(Json);
+        }
         public int? TruckTransferInitiationID { get; set; }
         [Required]
         public TruckModel Truck { get; set; }
         [Required]
-        public BranchModel InitiationBranch { get; set; }        
+        public BranchModel InitiationBranch { get; set; }
         public BranchModel DestinationBranch { get; set; }
         [Required]
         public EventModel TruckEvent { get; set; } = new();
@@ -17,14 +24,17 @@ namespace ArmsModels.BaseModels
         public TruckTransferEndModel TruckTransferEndModel { get; set; } = new();
         public int IstruckReject { get; set; } = 0;
     }
+
     public class TruckTransferEndModel
     {
-        public int? TruckTransferEndID { get; set; }        
-        public int? BranchID { get; set; }              
+        public int? TruckTransferEndID { get; set; }
+        public int? BranchID { get; set; }
         public bool? TransferStatus { get; set; }
         public EventModel TruckEvent { get; set; } = new();
         public string Remarks { get; set; }
-        public string StatusText { get
+        public string StatusText
+        {
+            get
             {
                 if (TransferStatus == null)
                     return "Being Transferred";
