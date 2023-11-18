23070eb9c36de91e9c4c2b609e9c7aeb8bbf7aaf
ROHITH MP <rohithmp2014@gmail.com>
2023-11-18 09:06:29 +0530

    OpTran implemented with permission key.

--- ArmsModels/IDataServices/Finance/Transactions/IOpTranService.cs
+++ ArmsModels/IDataServices/Finance/Transactions/IOpTranService.cs
@@ -13,15 +13,15 @@ namespace ArmsServices.DataServices
 {
     public interface IOpTranService
     {
-        OpTranModel Update(OpTranModel model);
-        int Delete(long? ID, string UserID);
+        OpTranModel Update(OpTranModel model);  //Edit
+        int Delete(long? ID, string UserID);  //Delete
         IEnumerable<OpTranModel> SelectByTrip(long? TripID);
         IEnumerable<OpTranModel> SelectByApprovedTrip(int? BranchID, long? TripID, int? NumberOfRecords, string searchTerm);
         IEnumerable<OpTranModel> SelectByUnapprovedTrip(int? BranchID, long? TripID, int? NumberOfRecords, string searchTerm);
         IEnumerable<OpTranModel> SelectByJobcard(int? JobcardID);
         OpTranModel SelectByID(long? ID);
-        int Approve(int? ID, string UserID, string Remarks);
-        int Reverse(int? ID, string UserID, string Remarks);
+        int Approve(int? ID, string UserID, string Remarks);  //Approve
+        int Reverse(int? ID, string UserID, string Remarks);  //Reverse
         IEnumerable<OpTranSubModel> GetExpenses(long? TransactionID);
     }
 }
