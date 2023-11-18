8fa19d10e038b925785ebe11e241a89715f35a5a
ROHITH MP <rohithmp2014@gmail.com>
2023-11-18 11:49:43 +0530

    Journal implemented with permission key.

--- ArmsModels/IDataServices/Finance/Transactions/IJournalService.cs
+++ ArmsModels/IDataServices/Finance/Transactions/IJournalService.cs
@@ -1,7 +1,4 @@
-﻿
-
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Data;
@@ -9,22 +6,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using ArmsModels.BaseModels;
 
-
-
 namespace ArmsServices.DataServices
 {
     public interface IJournalService
     {
-        JournalModel Update(JournalModel model);
+        JournalModel Update(JournalModel model);  //Edit
         JournalModel SelectByID(int? ID);
-        int Delete(int? ID, string UserID);
+        int Delete(int? ID, string UserID);  //Delete
         IEnumerable<JournalModel> Select(int? BranchID);
         IEnumerable<JournalModel> SelectByApproved(int? BranchID, int? NumberOfRecords, string searchTerm);
         IEnumerable<JournalModel> SelectByUnapproved(int? BranchID, int? NumberOfRecords, string searchTerm);
         IEnumerable<JournalModel> SelectByPeriod(DateTime? begin, DateTime? end);       
-        int Approve(int? ID, string UserID, string Remarks);
-        int Reverse(int? ID, string UserID, string Remarks);
+        int Approve(int? ID, string UserID, string Remarks);  //Approve
+        int Reverse(int? ID, string UserID, string Remarks);  //Reverse
         public IEnumerable<JournalSubModel> GetSubList(int? JournalID);
     }
 }
-
