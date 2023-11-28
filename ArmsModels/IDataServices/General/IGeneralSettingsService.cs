5edcef49ab8c604b50626b05eec94b4e78277ae4
ROHITH MP <rohithmp2014@gmail.com>
2023-11-28 09:55:42 +0530

    Admin => GeneralSettings * Implemented option which effects the whole application.

---
+++ ArmsModels/IDataServices/General/IGeneralSettingsService.cs
@@ -0,0 +1,11 @@
+﻿using ArmsModels.BaseModels.General;
+using System.Collections.Generic;
+
+namespace ArmsServices.DataServices
+{
+    public interface IGeneralSettingsService
+    {
+        IEnumerable<GeneralSettingsModel> Select();
+        void Update(GeneralSettingsModel model);
+    }
+}
