10addf51a79ac1643252ae5115b08698a52d0657
Rijas Laptop <mkrijas@gmail.com>
2024-02-07 17:41:05 +0530

    Navemenu working

---
+++ Views/Data/NavmenuModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using MudBlazor;
+
+namespace Views.Data
+{
+    public class NavMenuModel
+    {
+        public int ID { get; set; }
+        public bool IsGroup { get; set; }
+        public string Title { get; set; }
+        public string ParentTitle { get; set; }
+        public string href { get; set; }
+        public string Icon { get; set; }
+        public Color IconColor { get; set; }
+        public bool Expanded { get; set; } = false;
+        public string Description { get; set; }
+        public List<NavMenuModel> navItems { get; set; }
+        public bool Visible { get; set; } = true;
+    }
+
+
+
+}
