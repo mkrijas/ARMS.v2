a13e5d3e670cb10a3fa2cad5e557eb951ac9d638
mkrijas <mkrijas@gmail.com>
2024-02-29 01:09:32 +0530

    Repair work Remodelled

--- ArmsServices/DataServices/User/RoleService.cs
+++ ArmsServices/DataServices/User/RoleService.cs
@@ -160,23 +160,23 @@ namespace ArmsServices.DataServices
         public async Task<bool> HasClaim(string DocTypeID, string ClaimValue, CancellationToken cancellationToken = default)
         {
             cancellationToken.ThrowIfCancellationRequested();
-
             var authprov = await auth.GetAuthenticationStateAsync();
-            string RoleID = "";
-            if (authprov.User.Claims.Any(x => x.Type == ClaimTypes.Role))
-                RoleID = authprov.User.Claims.First(x => x.Type == ClaimTypes.Role).Value;
+           // string RoleID = "";
+            //if (authprov.User.Claims.Any(x => x.Type == ClaimTypes.Role))
+            //    RoleID = authprov.User.Claims.First(x => x.Type == ClaimTypes.Role).Value;
 
-            List<SqlParameter> parameters = new List<SqlParameter>
-            {
-               new SqlParameter("@RoleID", RoleID)
-            };
+            //List<SqlParameter> parameters = new List<SqlParameter>
+            //{
+            //   new SqlParameter("@RoleID", RoleID)
+            //};
 
-            IList<Claim> Claims = new List<Claim>();
-            foreach (IDataRecord dr in Iservice.GetDataReader("[usp.User.RoleClaims.Select]", parameters))
-            {
-                Claims.Add(new Claim(dr.GetString("ClaimType"), dr.GetString("ClaimValue")));
-            };
-            return Claims.Any(s => s.Type == DocTypeID && s.Value == ClaimValue);
+            //IList<Claim> Claims = new List<Claim>();
+
+            //foreach (IDataRecord dr in Iservice.GetDataReader("[usp.User.RoleClaims.Select]", parameters))
+            //{
+            //    Claims.Add(new Claim(dr.GetString("ClaimType"), dr.GetString("ClaimValue")));
+            //};
+            return authprov.User.Claims.Any(s => s.Type == DocTypeID && s.Value == ClaimValue);
         }
 
         public Task AddClaimAsync(RoleModel role, Claim claim, CancellationToken cancellationToken = default)
