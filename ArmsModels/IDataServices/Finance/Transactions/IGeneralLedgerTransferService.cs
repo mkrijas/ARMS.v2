4327297b224f9955311bb5dd863fe38022b5bd2a
Hafis <hafizappu1@gmail.com>
2024-05-27 17:32:13 +0530

    new general ledger transfer

---
+++ ArmsModels/IDataServices/Finance/Transactions/IGeneralLedgerTransferService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using ArmsModels.BaseModels;
+
+namespace ArmsServices.DataServices
+{
+    public interface IGeneralLedgerTransferService : IbaseInterface<GeneralLedgerTransferModel>
+    {
+       
+    }
+}
