42770094eccc1bb304a1995707ab4ad4797c9402
Hafis <hafizappu1@gmail.com>
2024-06-10 11:30:11 +0530

    usex item option in fuel entry

--- ArmsModels/BaseModels/Operations/TripModel.cs
+++ ArmsModels/BaseModels/Operations/TripModel.cs
@@ -90,6 +90,7 @@ namespace ArmsModels.BaseModels
         public int? Dimension { get; set; }
         public string Narration { get; set; }
         public string RefInvoiceNo { get; set; }
+        public bool IsUsedItem { get; set; }
     }
 
     public class TripAdvanceModel
