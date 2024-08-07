dda9883bab6a13128ea1758212b3c7aabdfaaf77
Hafis <hafizappu1@gmail.com>
2024-08-07 13:23:41 +0530

    sale view collection managed

--- ArmsModels/IDataServices/Finance/Transactions/ISaleService.cs
+++ ArmsModels/IDataServices/Finance/Transactions/ISaleService.cs
@@ -16,8 +16,8 @@ namespace ArmsServices.DataServices
         SaleModel SelectByID(int? ID);
         int Delete(int? ID, string UserID);  //Delete
         IEnumerable<SaleModel> Select();
-        IEnumerable<SaleModel> SelectByApproved(int? BranchID, int? NumberOfRecords, string searchTerm);
-        IEnumerable<SaleModel> SelectByUnapproved(int? BranchID, int? NumberOfRecords, string searchTerm);
+        IEnumerable<SaleModel> SelectByApproved(int? BranchID, int? NumberOfRecords, string searchTerm, string Type);
+        IEnumerable<SaleModel> SelectByUnapproved(int? BranchID, int? NumberOfRecords, string searchTerm, string Type);
         IEnumerable<SaleModel> SelectByParty(int? PartyID);
         IEnumerable<SaleModel> SelectByPeriod(DateTime? begin, DateTime? end);
         IEnumerable<TaxPurchaseExpenseModel> GetParticulars(int? SID);
--- ArmsServices/DataServices/Finance/Transactions/SaleService.cs
+++ ArmsServices/DataServices/Finance/Transactions/SaleService.cs
@@ -194,14 +194,15 @@ namespace ArmsServices.DataServices
             }
         }
 
-        public IEnumerable<SaleModel> SelectByApproved(int? BranchID, int? NumberOfRecords, string searchTerm)
+        public IEnumerable<SaleModel> SelectByApproved(int? BranchID, int? NumberOfRecords, string searchTerm, string Type)
         {
             List<SqlParameter> parameters = new List<SqlParameter>
             {
                new SqlParameter("@Operation", "ByApproved"),
                new SqlParameter("@BranchID", BranchID),
                new SqlParameter("@numberOfRecords", NumberOfRecords),
-               new SqlParameter("@searchTerm", searchTerm)
+               new SqlParameter("@searchTerm", searchTerm),
+               new SqlParameter("@Type", Type)
             };
 
             foreach (IDataRecord dr in Iservice.GetDataReader("[usp.Finance.Transactions.Sales.Select]", parameters))
@@ -210,14 +211,15 @@ namespace ArmsServices.DataServices
             }
         }
 
-        public IEnumerable<SaleModel> SelectByUnapproved(int? BranchID, int? NumberOfRecords, string searchTerm)
+        public IEnumerable<SaleModel> SelectByUnapproved(int? BranchID, int? NumberOfRecords, string searchTerm , string Type)
         {
             List<SqlParameter> parameters = new List<SqlParameter>
             {
                new SqlParameter("@Operation", "ByUnapproved"),
                new SqlParameter("@BranchID", BranchID),
                new SqlParameter("@numberOfRecords", NumberOfRecords),
-               new SqlParameter("@searchTerm", searchTerm)
+               new SqlParameter("@searchTerm", searchTerm),
+               new SqlParameter("@Type", Type)
             };
 
             foreach (IDataRecord dr in Iservice.GetDataReader("[usp.Finance.Transactions.Sales.Select]", parameters))
