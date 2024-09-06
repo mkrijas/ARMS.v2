a35949075f2bf28b3612487698205866a999a845
mkrijas <mkrijas@gmail.com>
2024-09-06 20:45:18 +0530

    Creidt Transfer Core Created

---
+++ ArmsModels/BaseModels/Finance/Transactions/CreditTransferModel.cs
@@ -0,0 +1,37 @@
+﻿using ArmsModels.BaseModels;
+using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArmsModels.BaseModels
+{
+    public class CreditTransferModel : TransactionBaseModel, ICloneable
+    {
+        public object Clone()
+        {
+            string Json = JsonConvert.SerializeObject(this);
+            return JsonConvert.DeserializeObject<CreditTransferModel>(Json);
+        }
+
+        public int? ID { get; set; }
+        [Required]
+        public int? TransactionMode { get; set; } //  -1 - Credit, 1 - Debit
+        [Required]
+        public string OriginPartyType { get; set; } // Supplier/Customer/Renter/SisterConcern
+        [Required]
+        public string OriginSubArdCode { get; set; }
+        [Required]
+        public int? OriginPartyID { get; set; }
+        [Required]
+        public int? OriginCoaID { get; set; }
+        [Required]
+        public string TargetPartyType { get; set; } // Supplier/Customer/Renter/SisterConcern
+        [Required]
+        public string TargetSubArdCode { get; set; }
+        [Required]
+        public int? TargetPartyID { get; set; }
+        [Required]
+        public int? TargetCoaID { get; set; }
+        
+    }
+}
