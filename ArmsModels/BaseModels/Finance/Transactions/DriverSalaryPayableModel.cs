dea3b0df83ab20f459fa96aa8f01365114e60111
Hafis <hafizappu1@gmail.com>
2024-10-16 14:40:36 +0530

    damage  text field error

---
+++ ArmsModels/BaseModels/Finance/Transactions/DriverSalaryPayableModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace ArmsModels.BaseModels
+{
+    public class DriverSalaryPayableModel : TransactionBaseModel, IValidatableObject, ICloneable
+    {
+        public object Clone()
+        {
+            string Json = JsonConvert.SerializeObject(this);
+            return JsonConvert.DeserializeObject<DriverSalaryPayableModel>(Json);
+        }
+        public DriverSalaryPayableModel()
+        {
+            NatureOfTransaction = "DriverSalaryPayable";
+        }
+        public int? ID { get; set; }
+        [Required]
+        public DateTime? FromDate { get; set; } = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+        [Required]
+        public DateTime? ToDate { get; set; } = DateTime.Today;
+        public List<DriverSalaryPayableListModel> DriversLists { get; set; } = new();
+    }
+
+    public class DriverSalaryPayableListModel
+    {
+        public int ID { get; set; }
+        public int? HeaderID { get; set; }
+        [Required]
+        public int DriverID { get; set; }
+        [Required]
+        public decimal? WorkDays { get; set; }
+        [Required]
+        public int NoOfTrips { get; set; }
+        [Required]
+        public int RunKM { get; set; }
+        [Required]
+        public decimal? GrossSalary { get; set; } = 0;
+        [Required]
+        public decimal? Deduction { get; set; } = 0;
+        [Required]
+        public decimal? NetSalary { get; set; } = 0;
+        public string Reference { get; set; }
+    }
+
+}
