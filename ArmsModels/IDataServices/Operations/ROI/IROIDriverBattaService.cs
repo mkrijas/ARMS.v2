92778380b6a68bcace53798a6b28bed4437753a7
Hafis <hafizappu1@gmail.com>
2024-10-29 12:03:58 +0530

    ROI Driver batta

---
+++ ArmsModels/IDataServices/Operations/ROI/IROIDriverBattaService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using ArmsModels.BaseModels;
+
+
+namespace ArmsServices.DataServices
+{
+    public interface IROIDriverBattaService
+    {
+        ROIDriverBattaModel Update(ROIDriverBattaModel model);
+        IEnumerable<ROIDriverBattaModel> Select();
+        ROIDriverBattaModel SelectByID(int? ID);
+    }
+}
