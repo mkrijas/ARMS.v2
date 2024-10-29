9079409eeef942dcfbed3d6ae76fb730215ee311
ROHITH MP <rohithmp2014@gmail.com>
2024-10-29 12:14:00 +0530

    IROICommonService implemented.

---
+++ ArmsModels/IDataServices/Operations/ROI/IROICommonService.cs
@@ -0,0 +1,10 @@
+﻿using Core.BaseModels.Operations.ROI;
+using System.Collections.Generic;
+
+namespace Core.IDataServices.Operations.ROI
+{
+    public interface IROICommonService
+    {
+        IEnumerable<ROITonnageModel> SelectBSType();
+    }
+}
