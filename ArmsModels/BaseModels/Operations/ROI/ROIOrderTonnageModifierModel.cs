64092c7424894912b34a45c56f54a3b70b4ea1d7
hafisappu <hafizappu1@gmail.com>
2025-03-28 17:11:29 +0530

    roi order tonnage

--- ArmsModels/BaseModels/Operations/ROI/ROIOrderTonnageModifierModel.cs
+++ ArmsModels/BaseModels/Operations/ROI/ROIOrderTonnageModifierModel.cs
@@ -2,6 +2,7 @@
 using ArmsModels.SharedModels;
 using Core.BaseModels.Finance.Transactions;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Core.BaseModels.Operations.ROI
@@ -16,12 +17,18 @@ namespace Core.BaseModels.Operations.ROI
         public int? ID { get; set; }
         [Required]
         public OrderModel Order { get; set; }
+        public string DisplayModifiers { get; set; }
+        public UserInfoModel UserInfo { get; set; } = new();
+        public List<ROIOrderTonnageModifierSubModel> modifiers { get; set; } = new();
+
+    }
+
+    public class ROIOrderTonnageModifierSubModel
+    {
         [Required]
         public byte? Wheels { get; set; }
         [Required]
         public int? Modifier { get; set; }
-        public UserInfoModel UserInfo { get; set; } = new();
-        public bool HideTextField { get; set; }
-
+        public virtual bool HideTextField { get; set; } = false;
     }
 }
--- ArmsModels/IDataServices/Operations/ROI/IROIOrderTonnageModifierService.cs
+++ ArmsModels/IDataServices/Operations/ROI/IROIOrderTonnageModifierService.cs
@@ -6,6 +6,7 @@ namespace Core.IDataServices.Operations.ROI
     public interface IROIOrderTonnageModifierService
     {
         IEnumerable<ROIOrderTonnageModifierModel> Select(int? RowNo, int? BranchID);
+        IEnumerable<ROIOrderTonnageModifierSubModel> SelectByOrder(int? RowNo, int? OrderID, int? BranchID);
         ROIOrderTonnageModifierModel Update(ROIOrderTonnageModifierModel model);
         int Delete(int? ID, string UserID);
     }
