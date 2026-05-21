82c72342fdcb6896906b2fa27008767bdfe695e0
hafis-thai <hafis@teamthai.in>
2026-05-21 14:54:03 +0530

    feat: add TyreModel and related inventory management data structures

--- ArmsModels/BaseModels/Inventory/TyreModel.cs
+++ ArmsModels/BaseModels/Inventory/TyreModel.cs
@@ -169,8 +169,8 @@ namespace ArmsModels.BaseModels
         public virtual decimal? GstRate { get; set; }
         public virtual decimal? TotalAmount { 
             get { 
-                if (TaxIncluded) return Amount + TDS; 
-                else return Amount + Tax + TDS; 
+                if (TaxIncluded) return Amount; 
+                else return Amount + Tax; 
             }
         }
     }
